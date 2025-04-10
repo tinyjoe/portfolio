689f92432c11dbba194a8f5954e43eff16972aaf
tinajoelly <tinaandrea.joelly2@edu.fh-kaernten.ac.at>
2025-04-10 22:52:46 +0200

    design site sections III

--- tina-joelly/src/app/main-content/hero-section/hero-section.component.ts
+++ tina-joelly/src/app/main-content/hero-section/hero-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MainButtonComponent } from '../../shared/main-button/main-button.component';
+import { HeroBannerComponent } from './hero-banner/hero-banner.component';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [MainButtonComponent],
+  imports: [MainButtonComponent, HeroBannerComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
--- tina-joelly/src/app/shared/footer/footer.component.ts
+++ tina-joelly/src/app/shared/footer/footer.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FooterLinkComponent } from '../footer-link/footer-link.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FooterLinkComponent],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
